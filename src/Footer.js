2381d2891b1d163305b242fd2bbc9572dbbd18c6
Nikita Naumov <naumovn808@gmail.com>
2023-07-22 08:17:02 +0500

    feat: start project

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href='#s'>home</a></li>
+                <li><a href='#s'>contacts</a></li>
+                <li><a href='#s'>about</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Footer;
