b96ab801cbad03a06a8d96257da61f3f03323576
Nikita Naumov <naumovnn808@gmail.com>
2023-08-07 20:34:49 +0500

    feat: add component Dialogs

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import './App.css'
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-
+import Dialogs from './components/Dialogs/Dialogs';
 
 export default function App() {
   return (
 
     <div className='app-wrapper'>
+
       <Header />
 
       <Navbar />
 
-      <Profile />
+      <div className='app-wrapper-content'>
+
+        <Dialogs />
+
+      </div>
+
+      {/* <Profile /> */}
 
     </div>
   );
