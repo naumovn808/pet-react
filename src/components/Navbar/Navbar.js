6c070d6a16bb4fbe655e718a72ee139656717e03
Nikita Naumov <naumovnn808@gmail.com>
2023-08-30 08:28:38 +0500

    feat: add routing as Navlink

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import classes from './Navbar.module.css'
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className={classes.nav}>
             <ul className='nav__list'>
-                <li className={classes.nav__item}><a href='/profile' className={classes.nav__link}>Profile</a></li>
-                <li className={classes.nav__item}><a href='/dialogs' className={classes.nav__link}>Messages</a></li>
-                <li className={classes.nav__item}><a href='/news' className={classes.nav__link}>News</a></li>
-                <li className={classes.nav__item}><a href='Music' className={classes.nav__link}>Music</a></li>
-                <li className={classes.nav__item}><a href='Settings' className={classes.nav__link}>Settings</a></li>
+                <li className={classes.nav__item}><NavLink to='/profile' className = { navData => navData.isActive ? classes.active : classes.nav__link }>Profile</NavLink></li>
+                <li className={classes.nav__item}><NavLink to='/dialogs' className = { navData => navData.isActive ? classes.active : classes.nav__link }>Messages</NavLink></li>
+                <li className={classes.nav__item}><NavLink to='/news' className = { navData => navData.isActive ? classes.active : classes.nav__link }>News</NavLink></li>
+                <li className={classes.nav__item}><NavLink to='Music' className = { navData => navData.isActive ? classes.active : classes.nav__link }>Music</NavLink></li>
+                <li className={classes.nav__item}><NavLink to='Settings' className = { navData => navData.isActive ? classes.active : classes.nav__link }>Settings</NavLink></li>
             </ul>
         </nav>
     )
